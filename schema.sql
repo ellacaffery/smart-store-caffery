cadad4e8381dcd78b19a521ca4a1bd28ac05e8b3
Ella Caffery <ellacafferyy@gmail.com>
2025-03-31 10:57:30 -0500

    Added warehouse database dump

--- schema.sql
+++ schema.sql
@@ -1,11 +1,14 @@
 -- Create Fact Table (Sales)
 CREATE TABLE Sales (
-    sale_id INTEGER PRIMARY KEY,
-    product_id INTEGER,
+    transaction_id INTEGER PRIMARY KEY,
+    saledate INTEGER,
     customer_id INTEGER,
-    quantity_sold INTEGER,
-    total_price REAL,
-    sale_date DATE,
+    product_id INTEGER,
+    store_id REAL,
+    campaign_id DATE,
+    saleamount REAL,
+    discountpercentage REAL,
+    state TEXT,
     FOREIGN KEY (product_id) REFERENCES Products(product_id),
     FOREIGN KEY (customer_id) REFERENCES Customers(customer_id)
 );
@@ -15,13 +18,17 @@ CREATE TABLE Products (
     product_id INTEGER PRIMARY KEY,
     product_name TEXT,
     category TEXT,
-    price REAL
+    unitprice REAL
+    yearadded INTEGER,
+    storesection TEXT
 );
 
 -- Create Dimension Table (Customers)
 CREATE TABLE Customers (
     customer_id INTEGER PRIMARY KEY,
     name TEXT,
-    location TEXT,
-    age INTEGER
+    region TEXT,
+    joindate INTEGER,
+    loyaltypoints INTEGER,
+    preferredcontactmethod TEXT
 );
